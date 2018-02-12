54d129c3022788d646cf9fd056a5f56368e5887a
Ruihe (Jerry) Li <ruiheli@uw.edu>
2018-02-12 13:22:11 -0800

    done

---
+++ iQuiz/iQuiz/CustomTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CustomTableViewCell.swift
+//  iQuiz
+//
+//  Created by Jerry Li on 2/12/18.
+//  Copyright © 2018 Jerry Li. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellLabel: UILabel!
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellView: UIView!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
